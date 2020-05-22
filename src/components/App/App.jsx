771fe8a4b1dddbf2c3a4d1ca9f2c7878addbbd32
kpodmasko <podmasko.konstantin@gmail.com>
2020-05-22 08:24:01 +0300

    half-implemented products toggler

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import {faStar, faDollarSign} from '@fortawesome/free-solid-svg-icons'
 
 import Header from '../Header';
 import Toggler from "../Toggler";
 import Select from "../Select";
+import Icon from "../Icon";
 import {testIds} from "../../utils/constants";
 
 import './App.css';
 
 function App() {
+    const renderALTS = useCallback(({setIsOpen, isOpen}) => <div onClick={() => setIsOpen(!isOpen)}>
+        ALTS
+    </div>, []);
+
+    const renderJSD = useCallback(({setIsOpen, isOpen}) => <div onClick={() => setIsOpen(!isOpen)}>
+        JSD
+        <Icon icon={faDollarSign}/>
+    </div>, []);
+
     return <div data-testid={testIds.app} className='app'>
         <Header title='Market' className='app__header'/>
         <Toggler>
-            <Select>
-                asd
+            <Icon icon={faStar}/>
+            <span>Margin</span>
+            <span>BNB</span>
+            <span>BTC</span>
+            <Select render={renderALTS}>
+                altsopen
             </Select>
-            <Select>
-                ssd
+            <Select render={renderJSD}>
+                jsdopen
             </Select>
         </Toggler>
     </div>
