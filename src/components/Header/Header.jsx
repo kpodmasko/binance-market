bc8eb1a3be0eca0fbf475e75a5f20a5031e301b9
kpodmasko <podmasko.konstantin@gmail.com>
2020-05-22 07:04:46 +0300

    refactored selectors in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header({title = '', className = ''}) {
     const classes = `header ${className}`;
 
     return <header className={classes} data-testid={testIds.header}>
-        <h1>{title}</h1>
+        <h1 className='header__title'>{title}</h1>
     </header>
 }
 
