64cb1f4e8a0582d87e0d857df807019f7b0678fe
kpodmasko <podmasko.konstantin@gmail.com>
2020-05-22 08:12:51 +0300

    beautified App content

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
 import Header from '../Header';
+import Toggler from "../Toggler";
+import Select from "../Select";
 import {testIds} from "../../utils/constants";
 
 import './App.css';
 
 function App() {
-    return <div data-testid={testIds.app}>
+    return <div data-testid={testIds.app} className='app'>
         <Header title='Market' className='app__header'/>
+        <Toggler>
+            <Select>
+                asd
+            </Select>
+            <Select>
+                ssd
+            </Select>
+        </Toggler>
     </div>
 }
 
