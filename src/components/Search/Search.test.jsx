4bdc2d83005430611285f7b11f8eb8c8b019c8a1
kpodmasko <podmasko.konstantin@gmail.com>
2020-05-23 09:10:23 +0300

    fixed typos in Search test

--- src/components/Search/Search.test.jsx
+++ src/components/Search/Search.test.jsx
@@ -6,13 +6,13 @@ import {testIds} from "../../utils/constants";
 
 test('renders search', () => {
     const {getByTestId} = render(<Search/>);
-    const iconElement = getByTestId(testIds.search);
-    expect(iconElement).toBeInTheDocument();
+    const searchElement = getByTestId(testIds.search);
+    expect(searchElement).toBeInTheDocument();
 });
 
 test('handles prop className', () => {
     const testClassname = 'test-classname';
     const {getByTestId} = render(<Search className={testClassname}/>);
-    const iconElement = getByTestId(testIds.search);
-    expect(iconElement.classList.contains(testClassname)).toBe(true);
+    const searchElement = getByTestId(testIds.search);
+    expect(searchElement.classList.contains(testClassname)).toBe(true);
 });
